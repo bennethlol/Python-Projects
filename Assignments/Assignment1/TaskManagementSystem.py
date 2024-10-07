9c8b4c70501988d53abf965f0412239a65148e86
bennethlol <2006K.Hudson@gmail.com>
2024-10-07 10:35:57 -0400

    Program Introduction: Creation of the introduction. Includes two print statements that explain what the program is and how it works.

---
+++ Assignments/Assignment1/TaskManagementSystem.py
@@ -0,0 +1,7 @@
+# Author: Kenneth Hudson
+# Date: 10/07/2024 (MM/DD/YY)
+# A program that lets you add and manage tasks.
+
+# Small introduction to the program
+print('Welcome to the task management system!')
+print('Here you can upload and manage your tasks! You can also set the priority and mark your completed tasks!')
--- Assignments/TaskManagementSystem.py
+++
@@ -1,3 +0,0 @@
-# Author: Kenneth Hudson
-# Date: 10/07/2024 (MM/DD/YY)
-# A program that lets you add and manage tasks.

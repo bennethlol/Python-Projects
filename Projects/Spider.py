378d6a897c1d90a9aec9ed095cde2f8c5112159b
bennethlol <2006K.Hudson@gmail.com>
2024-10-10 11:56:49 -0400

    Classes practice

--- Projects/Spider.py
+++ Projects/Spider.py
@@ -1,9 +1,20 @@
 class Animal:
-    def __init__(self, arms, legs):
+    def __init__(self, arms, legs, speed):
         self._arms = arms
         self._legs = legs
+        self._speed = 5
     def limbs(self):
         return self._arms + self._legs
+    def compareSpeed(self, animal2):
+       return self._speed > animal2._speed
+           
 
-spider = Animal(4, 4)
+spider = Animal(4, 4, 10)
+millipede = Animal(100, 200, 15)
 print(f'Total limbs are {spider.limbs()}')
+print(f'Total limbs are {millipede.limbs()}')
+
+if spider.compareSpeed(millipede):
+    print('Spiders are faster')
+else:
+    print('Millipedes are faster')

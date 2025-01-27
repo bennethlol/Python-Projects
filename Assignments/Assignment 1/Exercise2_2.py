156b7b08a30597aabe7a5d081e4db063c573eeee
bennethlol <2006K.Hudson@gmail.com>
2025-01-26 21:19:08 -0500

    ThinkPythonEx2.12 – Exercise 2.2

---
+++ Assignments/Assignment 1/Exercise2_2.py
@@ -0,0 +1,15 @@
+width = 17
+height = 12.0
+delimiter = '.'
+
+a = width/2
+print(type(a))
+b = width/2.0
+print(type(b))
+c = height/3
+print(type(c))
+d = 1+2*5
+print(type(d))
+e = delimiter*5
+print(type(e))
+

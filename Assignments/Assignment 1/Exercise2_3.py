621396baed12a624f2d09e490c0c1c69a11ff8be
bennethlol <2006K.Hudson@gmail.com>
2025-01-26 21:40:27 -0500

    ThinkPythonEx2.12 - Exercise 2.3

---
+++ Assignments/Assignment 1/Exercise2_3.py
@@ -0,0 +1,20 @@
+# Question 1
+r = 5
+pi = 3.14
+v = (4/3)*(pi)*(r**3)
+print('Volume of the sphere is:',v)
+
+# Question 2
+book = 24.95
+bookdis = book - (book*0.40)
+wholesale = (bookdis + 3) + ((bookdis + 0.75)*59)
+print('Total wholesale cost of 60 books is:',wholesale,'dollars.')
+
+# Question 3
+easy = 8.15
+tempo = 7.12
+print(easy*2)
+print(tempo*3)
+# The current time is 6:52 AM. According to the python interpretor, we have run for a total of 38 minutes and 6 seconds.
+# Therefore, you will reach home by 7:30 AM.
+

74f617c23ca90d8d26e6940d39f19c67d4abaebb
bennethlol <2006K.Hudson@gmail.com>
2025-02-03 18:22:18 -0500

    c

--- Projects (NEW)/Week 2 Activites/Activity2Week2.py
+++ Projects (NEW)/Week 2 Activites/Activity2Week2.py
@@ -7,4 +7,5 @@ print(a+b/c**d)
 
 # Observations:
 # The print statement is the same as 10 + (4/2^3) if you were doing regular math.
-# The reason why output is 10.5 is because it does exponent first, then division, then addition/subtraction.
+# The reason why the output is 10.5 is because this is the correct order of operations 
+# for mathematics (i.e., exponent first, then division, then addition/subtraction)

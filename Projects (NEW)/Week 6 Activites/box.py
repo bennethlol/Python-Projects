69154a237ac8a3e024bea1178d7badee7ef207f9
bennethlol <2006K.Hudson@gmail.com>
2025-02-17 17:12:58 -0500

    Week 6

---
+++ Projects (NEW)/Week 6 Activites/box.py
@@ -0,0 +1,11 @@
+class Box:
+    def __init__(self):
+        self._pencil = 'pencil'
+        self._ruler = 'ruler'
+        self._calculator = 'calculator'
+    def getPencil(self):
+        return self._pencil
+    def getRuler(self):
+        return self._ruler
+    def getCalculator(self):
+        return self._calculator
---
+++ Projects (NEW)/Week 6 Activites/geo.py
@@ -0,0 +1,10 @@
+from box import Box
+class Program:
+    def run(self):
+        b = Box()
+        b.getPencil()
+        b.getRuler()
+        b.getCalculator()
+
+prog = Program()
+prog.run()

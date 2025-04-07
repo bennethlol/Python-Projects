676f0b781b4691ef81da3c13a5d210dc2a455333
bennethlol <2006K.Hudson@gmail.com>
2025-04-06 20:44:49 -0400

    Version 2

--- Assignments/Assignment 4/PhoneRec.py
+++ Assignments/Assignment 4/PhoneRec.py
@@ -1,11 +1,18 @@
+import json
+
 # Phonebook dictionary
 phonebook = {  
     'John': ('209 Trafalgar Road', '905-666-7777'),  
     'Rosie': ('1439 Trafalgar Road', '487-423-7721')  
 }
 
-# Open file "SpeedDial1.txt"
-with open("SpeedDial1.txt", "w") as file:
-    # Write each phone number on a new line
-    for name, (add,phone) in phonebook.items():
-        file.write(phone + "\n")
+# Convert into json file
+with open("SpeedDial2.json", "w") as file:
+    json.dump(phonebook, file)
+
+# Open file "SpeedDial2.json"
+with open("SpeedDial2.json", "r") as file:
+    load = json.load(file)
+    # Print details
+    for name, (add,phone) in load.items():
+        print(f"{name}'s Address: {add}, Phone Number: {phone}")
